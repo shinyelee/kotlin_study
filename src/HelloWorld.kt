2dfbcfade7ffb48871efe2b64b5c082f83ddcd27
shinyelee <shinye0213@naver.com>
2022-02-22 17:54:24 +0900

    val & var

--- src/HelloWorld.kt
+++ src/HelloWorld.kt
@@ -1,3 +1,17 @@
 fun main(args: Array<String>){
+
     println("Hello World")
+
+    // val -> 변수값 변경 불가
+    val asdf = "test1"
+    println(asdf)
+    val box2 = "test2"
+    println(box2)
+
+    // var -> 변수값 변경 가능
+    var box3 = "test3"
+    println(box3)
+    box3 = "test4"
+    println(box3)
+
 }
