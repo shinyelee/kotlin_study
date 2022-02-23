a1758ac5fe223eb71f0b1c4b2fb86abf2b08b6ce
shinyelee <shinye0213@naver.com>
2022-02-24 00:27:00 +0900

    map

--- src/Basic4.kt
+++ src/Basic4.kt
@@ -5,21 +5,52 @@
 
 fun main () {
 
-    // list는 추가/제거를 못 함
-    val testList1 = listOf("a", "b", "c")
-    println(testList1)
-    println(testList1[0])
-    // 이렇게 만들면 추가/제거 불가능
+//    // list는 추가/제거를 못 함
+//    val testList1 = listOf("a", "b", "c")
+//    println(testList1)
+//    println(testList1[0])
+//    // 이렇게 만들면 추가/제거 불가능
 //    testList1.add("d")
+//
+//    // mutable list는 추가/제거 가능
+//    val testList2 = mutableListOf("a", "b", "c")
+//    println(testList2)
+//    // "d" 추가
+//    testList2.add("d")
+//    println(testList2)
+//    // "a" 제거
+//    testList2.remove("a")
+//    println(testList2)
 
-    // mutable list는 추가/제거 가능
-    val testList2 = mutableListOf("a", "b", "c")
-    println(testList2)
-    // "d" 추가
-    testList2.add("d")
-    println(testList2)
-    // "a" 제거
-    testList2.remove("a")
-    println(testList2)
+    // 5 유리
+    // 10 철수
+    // 15 짱구
+    // 22 훈이
+//
+//    val mutableList1 = mutableListOf<Int>()
+//    mutableList1.add(5)
+//    mutableList1.add(10)
+//    mutableList1.add(15)
+//    mutableList1.add(22)
+//
+//    val mutableList2 = mutableListOf<String>()
+//    mutableList2.add("유리")
+//    mutableList2.add("철수")
+//    mutableList2.add("짱구")
+//    mutableList2.add("훈이")
+//
+//    val findIndex = mutableList1.indexOf(10)
+//    println(findIndex)
+//    println(mutableList2[findIndex])
+
+    // 인덱스는 겁나 귀찮음 -> map 사용
+    val testMap1 = mutableMapOf<Int, String>()
+    testMap1.put(5, "유리")
+    testMap1.put(10, "철수")
+    testMap1.put(15, "짱구")
+    testMap1.put(22, "훈이")
+
+    println(testMap1[15])
 
 }
+
