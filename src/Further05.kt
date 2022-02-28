ff6eb62fee7a7f63f89a85773e1064e84e8b04e0
shinyelee <shinye0213@naver.com>
2022-02-28 16:44:34 +0900

    overloading

---
+++ src/Further05.kt
@@ -0,0 +1,36 @@
+// OverLoading
+
+fun main() {
+
+    val c = Calculator()
+//    c.sumNumberTwo(1, 2)
+//    c.sumNumberThree(1, 2, 3)
+    c.sumNumber(1, 2)
+    c.sumNumber(1, 2, 3)
+    c.sumNumber("나는 ", "행복합니다")
+
+}
+
+class Calculator() {
+
+//    fun sumNumberTwo(a : Int, b : Int) {
+//        println(a + b)
+//    }
+
+//    fun sumNumberThree(a : Int, b : Int, c : Int) {
+//        println(a + b + c)
+//    }
+
+    fun sumNumber(a : Int, b : Int) {
+        println(a + b)
+    }
+
+    fun sumNumber(a : Int, b : Int, c : Int) {
+        println(a + b + c)
+    }
+
+    fun sumNumber(a : String, b : String) {
+        println(a + b)
+    }
+
+}
