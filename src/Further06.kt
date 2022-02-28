c482fb08c84dcbea199d92113db16882242ba688
shinyelee <shinye0213@naver.com>
2022-02-28 16:50:12 +0900

    overloading

---
+++ src/Further06.kt
@@ -0,0 +1,34 @@
+// Inheritance
+
+fun main() {
+
+    Job1()
+    Job2()
+    Job3()
+}
+
+open class AllJobs() {
+    init {
+        println("일을 합니다")
+    }
+}
+
+class Job3() : AllJobs() {
+    init {
+        println("마케팅을 합니다")
+   }
+}
+
+class Job1() {
+    init {
+        println("일을 합니다")
+        println("코딩을 합니다")
+    }
+}
+
+class Job2() {
+    init {
+        println("일을 합니다")
+        println("디자인을 합니다")
+    }
+}
