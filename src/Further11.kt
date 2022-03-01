acaf9ea791c779be5df799fd53e18d1c38314180
shinyelee <shinye0213@naver.com>
2022-03-01 15:04:01 +0900

    Inner Class

--- src/Further11.kt
+++ src/Further11.kt
@@ -1,16 +1,29 @@
-// Nested Class(중첩 클래스)
-// 객체지향 / 캡슐화
-
 fun main() {
 
-    val test1 = Test1.Test1NestedClass()
-    test1.testFun1()
+    val test4 = Test4.Test4NestedClass()
+    test4.testFun4()
+
+    val test5 = Test5().Test5InnerClass()
+    test5.testFun5()
+
 }
 
-class Test1 {
-    class Test1NestedClass {
-        fun testFun1() {
-            println("TestFun1")
+// Nested Class(중첩 클래스)
+// 객체지향 / 캡슐화
+class Test4 {
+    class Test4NestedClass {
+        fun testFun4() {
+            println("TestFun4")
+        }
+    }
+}
+
+// Inner Class(내부 클래스)
+// RecyclerView
+class Test5 {
+    inner class Test5InnerClass {
+        fun testFun5() {
+            println("TestFun5")
         }
     }
 }
