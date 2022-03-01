bb4af3636a5754b1cd25a6abf2226142be5f668a
shinyelee <shinye0213@naver.com>
2022-03-01 14:17:01 +0900

    interface

---
+++ src/Further09.kt
@@ -0,0 +1,64 @@
+// 인터페이스(interface)
+
+fun main() {
+
+    BMW3().wheel()
+    BMW3().engine()
+    BMW3().autoDriving()
+    BMW3().autoParking()
+
+}
+
+//interface Car2 {
+//    fun wheel()
+//    fun engine()
+//}
+//
+//class BMW2() : Car2 {
+//    override fun wheel() {
+//        println("wheel 돌아감")
+//    }
+//    override fun engine() {
+//        println("엔진 돌아감")
+//    }
+//}
+
+abstract class Car3 {
+    abstract fun wheel()
+    abstract fun engine()
+}
+
+interface CarAutoDriving {
+    fun autoDriving()
+}
+
+interface CarAutoParking {
+    fun autoParking()
+}
+
+class BMW3() : Car3(), CarAutoDriving, CarAutoParking {
+    override fun wheel() {
+        println("wheel 돌아감")
+    }
+    override fun engine() {
+        println("엔진 돌아감")
+    }
+    override fun autoDriving() {
+        println("BMW3 자율주행")
+    }
+    override fun autoParking() {
+        println("BMW 자동주차")
+    }
+}
+
+class Benz3() : Car3() {
+    override fun wheel() {
+        println("wheel 돌아감")
+    }
+    override fun engine() {
+        println("엔진 돌아감")
+    }
+    fun autoParking() {
+        println("Benz3 자동주차")
+    }
+}
