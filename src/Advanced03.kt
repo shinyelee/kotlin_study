2fe490b115d6e14bf9a67a42f4c1ee4a09450508
shinyelee <shinye0213@naver.com>
2022-03-10 20:35:41 +0900

    Generic (2)

--- src/Advanced03.kt
+++ src/Advanced03.kt
@@ -14,6 +14,35 @@ fun main() {
     val box1_1 = Box1("10".toInt())
     println(box1_1.value)
 
+    val box3 = Box3(10)
+    println(box3.value)
+
+    val box4 = Box3("10")
+    println(box4.value)
+
+    // 문자 123은 ㄱㅊ 숫자 123은 에러남
+//    testFun1("123")
+//    testFun1(123)
+
+    // 둘 다 문제없이 출력됨됨
+    testFun2("123")
+    testFun2(123)
+    
+    // 꼭 T가 아니라 다른 알파벳이어도 ㄱㅊ
+    testFun3("123")
+    testFun3(123)
+}
+
+fun testFun1(a : String) {
+    println(a)
+}
+
+fun <T> testFun2(a : T) {
+    println(a)
+}
+
+fun <A> testFun3(a : A) {
+    println(a)
 }
 
 class Box1(test : Int) {
@@ -21,5 +50,10 @@ class Box1(test : Int) {
 }
 
 class Box2(test : String) {
+    var value = test
+}
+
+// Generic
+class Box3<T>(test : T) {
     var value = test
 }
