04e6d93aa0b1693360d361c8e34bf1e4dfcf30ee
shinyelee <shinye0213@naver.com>
2022-03-10 23:35:30 +0900

    Object (3)

--- src/Advanced04.kt
+++ src/Advanced04.kt
@@ -25,6 +25,30 @@ fun main() {
     println(test3.count)
     println(test4.count)
 
+    val test5 = TestObjectClass()
+    val test6 = TestObjectClass()
+
+    test5.plusBtn()
+    println(TestObjectClass.number)
+    test5.plusBtn()
+    println(TestObjectClass.number)
+    test5.plusBtn()
+    println(TestObjectClass.number)
+    test6.minusBtn()
+    println(TestObjectClass.number)
+
+}
+
+class TestObjectClass {
+    companion object {
+        var number = 0
+    }
+    fun plusBtn() {
+        number++
+    }
+    fun minusBtn() {
+        number--
+    }
 }
 
 object testObject {
