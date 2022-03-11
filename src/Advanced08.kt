74f68cec50e24bf1637e2d042a2365b31ccaddb4
shinyelee <shinye0213@naver.com>
2022-03-11 22:59:13 +0900

    enum class (1)

---
+++ src/Advanced08.kt
@@ -0,0 +1,38 @@
+// enum(enumerations) class
+
+fun main() {
+
+    println(Direction.NORTH)
+    Direction.values().forEach {
+        println(it)
+    }
+
+    CustomerType.values().forEach {
+        println(it)
+    }
+
+    val direction = Direction.EAST
+    when(direction) {
+        Direction.NORTH -> {
+            println("N")
+        }
+        Direction.SOUTH -> {
+            println("S")
+        }
+        Direction.WEST -> {
+            println("W")
+        }
+        Direction.EAST -> {
+            println("E")
+        }
+    }
+
+}
+
+enum class Direction {
+    NORTH, SOUTH, WEST, EAST
+}
+
+enum class CustomerType {
+    A, B, C, D, VIP
+}
