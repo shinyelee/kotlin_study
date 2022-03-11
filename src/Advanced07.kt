bf2bfb46ac04601b8205e1adea79b9574801072d
shinyelee <shinye0213@naver.com>
2022-03-11 22:30:30 +0900

    scope function (3) run

--- src/Advanced07.kt
+++ src/Advanced07.kt
@@ -2,6 +2,8 @@
 
 fun main() {
 
+    // let
+    // non null일 때 동작O null일때 동작X
     val str : String? = "hi"
     println(str?.length)
     val length = str?.let {
@@ -10,8 +12,6 @@ fun main() {
     }
     println(length)
 
-    // let
-    // non null일 때 동작O null일때 동작X
     val str2 : String? = null
     val length2 = str2?.let {
         println(it)
@@ -30,4 +30,21 @@ fun main() {
     }
     println(firstAndLast)
 
+    // run
+    // 객체 초기화와 return값 계산이 필요할 때 주로 사용
+    val service = multiPortService("www.naver.com", 80)
+    val result1 = service.query(service.prepareRequest() + " to port ${service.port}")
+    println(result1)
+
+    val result2 = service.run {
+        port = 8080
+        query(prepareRequest() + " to port $port")
+    }
+    println(result2)
+
+}
+
+class multiPortService(var url : String, var port : Int) {
+    fun prepareRequest() : String = "기본 요청 url $url"
+    fun query(request : String) = "결과 query $request"
 }
