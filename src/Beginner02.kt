8e93a8288ee0f84a733a45ae304694598cb92b2b
shinyelee <shinye0213@naver.com>
2022-03-13 00:15:59 +0900

    strings

---
+++ src/Beginner02.kt
@@ -0,0 +1,18 @@
+// strings(문자열)
+
+fun main () {
+
+    // + 연산자를 사용해 두 문자열 결합하기
+    val language = "Kotlin"
+    val creator = "JetBrains"
+    val description = language + " is created by " + creator
+    println(description)
+//    Kotlin is created by JetBrains
+
+    // $ 사용해 문자열 결합하기(더 편리함)
+    val releaseDate = "July 2011"
+    val releaseString = "Kotlin was released in $releaseDate"
+    println(releaseString)
+//    Kotlin was released in July 2011
+
+}
