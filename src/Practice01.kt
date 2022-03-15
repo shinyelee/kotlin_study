2a8f6d764f9f52d27f86ff3aaab7dd4946b6aa8a
shinyelee <shinye0213@naver.com>
2022-03-15 14:22:28 +0900

    coding-test plus

---
+++ src/Practice01.kt
@@ -0,0 +1,19 @@
+// 숫자 2개 더하기 기능이 있는 solution 을 완성하세요
+
+fun main(){
+
+    // 예시
+    val result1 = solution(1,2)
+    // 결과
+    println(result1) // 3
+
+    val result2 = solution(2,3)
+    println(result2) // 5
+
+}
+
+fun solution(a : Int, b : Int) : Int {
+
+    return a + b
+
+}
