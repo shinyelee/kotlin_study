ed796121f6b868c68b18bb00d22d743dc69142e1
shinyelee <shinye0213@naver.com>
2022-03-29 22:00:09 +0900

    반복문과 증감연산자

--- src/Beginner09forWhile.kt
+++ src/Beginner09forWhile.kt
@@ -2,7 +2,12 @@
 
 fun main() {
 
+    // 증감연산자
+    // ++
+    // --
+
     // for
+    // 범위형 반복문 -> 범위를 정해 반복 수행
     // 리스트 반복 출력
     val cards1 = mutableListOf("Jack", "Queen", "King")
     for (card in cards1) {
@@ -19,6 +24,11 @@ fun main() {
         println(j)
     }
 
+    // 문자열도 가능
+    for (k in 'a'..'e') {
+        println(k)
+    }
+
     // 키, 값 동시 출력
     val cards2 = mapOf("Jack" to 11, "Queen" to 12, "King" to 13)
     for ((name, value) in cards2) {
@@ -26,7 +36,13 @@ fun main() {
     }
 
     // while
+    // 조건형 반복문 -> 조건이 참인 경우 반복 유지
     // 변수가 false 될 때까지 출력
+    var a = 0
+    while (a < 5) {
+        println(a++)
+    }
+
 //    while (stillDownloading) {
 //        println("Downloading…")
 //    }
