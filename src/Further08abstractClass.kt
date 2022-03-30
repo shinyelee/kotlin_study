f6d481b36e36ad7ca8d0954eda7803469afe3034
shinyelee <shinye0213@naver.com>
2022-03-30 20:33:37 +0900

    오버라이딩과 추상화 (2)

--- src/Further08abstractClass.kt
+++ src/Further08abstractClass.kt
@@ -1,4 +1,5 @@
 // Abstract(추상화)
+// 형식만 선언하고 실제 구현은 서브 클래스에 일임할 때 사용하는 기능
 
 // Abstraction Function(추상 함수)
 // 선언부만 있고 기능이 구현되지 않은 함수
@@ -12,6 +13,11 @@ fun main() {
 
     r.lead()
     r.response()
+
+    var d = Dogs()
+
+    d.run()
+    d.eat()
 
 }
 
@@ -32,5 +38,34 @@ class Carot : Game() {
     // eat 함수가 실제 동작되는 구현부
     override fun response() {
         println("당근 당근")
+    }
+}
+
+// Interface(인터페이스)
+// 서로 다른 기능들을 여러개 물려줘야 할 때 유용한 기능
+
+// 추상함수로만 이루어져있는 순수 추상화 기능
+// 속성 || 추상함수 || 일반함수 가질 수 있음
+// 생성자를 가질 수 없음
+
+// 구현부 X 함수 -> 자동으로 abstract 함수로 인식 -> abstract 생략
+interface Runner {
+    fun run()
+}
+
+// 구현부 O 함수 -> 자동으로 open 함수로 인식 -> open 생략
+interface Eater {
+    fun eat() {
+        println("음식을 먹습니다")
+    }
+}
+
+// 서브 클래스 하나가 한 번에 여러 인터페이스를 상속 받을 수 있음
+class Dogs : Runner, Eater {
+    override fun run() {
+        println("WADADA WADADA 달려")
+    }
+    override fun eat() {
+        println("like hubba-hubba")
     }
 }
