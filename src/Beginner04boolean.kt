4cd768535c612d727fd4a1ec1f294cfdb88d5089
shinyelee <shinye0213@naver.com>
2022-03-31 18:30:10 +0900

    논리형 보충설명

--- src/Beginner04boolean.kt
+++ src/Beginner04boolean.kt
@@ -1,11 +1,15 @@
-// Booleans(불린/불리언)
+// Booleans(불린/불리언/논리형)
 
 fun main() {
 
     // true 또는 false 값을 나타냄
+    // 연산자 배울 때 또 나옴
+
     val isGoodDay = true
     val isBadDay = false
+
     println(isGoodDay)
     println(isBadDay)
-
+//    true
+//    false
 }
