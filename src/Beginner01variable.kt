1bc26245a63e488aa94dd8fe6a1e0088215b70f7
shinyelee <shinye0213@naver.com>
2022-03-31 15:55:50 +0900

    변수 보충설명

--- src/Beginner01variable.kt
+++ src/Beginner01variable.kt
@@ -1,40 +1,36 @@
-// Variables(변수)
+// Variables(변수) //
 
-// 클래스명은 파스칼 표기법
-// 함수/변수명은 카멜케이스 표기법
+// 값을 저장하는 메모리 공간
 
 fun main() {
 
+    // Variable Declaration(변수 선언) //
+
     // var 변수명: 변수타입 = 변수값
-    // 의 형태로 작성
+    // 이런 형태로 작성
     var greeting: String = "Hello World!"
 
-    // 변수타입 생략O
-    var greeting2 = "Hello World!2"
-    // 이렇게만 써도 코틀린은 변수타입이 문자열이란 것을 앎
+    // : String 생략해도 OK
+    // (자세한 내용은 자료형에서 추가 설명)
+    var greeting2 = "Hello World!"
 
-    // 변수 수정시 var 생략O
+    // 변수를 초기화 할 때는 var 필수
+    // -> 값 수정할 때는 var를 생략
     var greeting3 = "Hello World!"
     greeting3 = "Hello Kotlin!"
 
-    // matable variable -> 가변 변수 -> var
-    // 일반적인 변수
-    // 언제든지 읽기/쓰기 가능
+    // 변수명은 camelCase(카멜표기법)으로 작성
+    // -> 아래와 같이 띄어쓰기를 대문자로 대체함
+    var permissionToDance = "We don't need permission to dance"
+    var dreamsComeTrue = "나를 지켜줄 거야 아껴왔던 작은 사랑도"
 
-    // immutable variable -> 불변 변수 -> val
-    // 선언시에만 초기화 가능
-    // 중간에 값 변경 불가
 
-    // 수정할 수 없는 변수를 만들고 싶다면 var 대신 val 사용
 
-    val greeting4 = "Hello World!"
-//    greeting4 = "Hello Kotlin!"
-//    Kotlin: Val cannot be reassigned
+    // 다음에 배울 내용 //
 
-    // 클래스에 선언된 변수 -> property(속성)
-    // 이외 스코프 내에 선언된 변수 -> Local Variable(지역변수)
-
-    var nullable: Int? = null
+    // 클래스에 선언된 변수 -> Property(속성)
+    // 스코프 내에 선언된 변수 -> Local Variable(지역 변수)
     // 자료형 뒤에 ? 붙여서 nullable 변수로 선언
+//    var nullable: Int? = null
 
 }
