55869ba3dfd7275a5bd0fff6d5744dc75abea6e3
shinyelee <shinye0213@naver.com>
2022-03-31 18:26:50 +0900

    문자형 보충설명

--- src/Beginner02charString.kt
+++ src/Beginner02charString.kt
@@ -1,14 +1,28 @@
-// Strings(문자열)
-
-// 큰따옴표 안에
-// "이런 식으로"
-// 사용
-
-// 여러 줄인 경우
-// """이런 식으로"""
-// 줄바꿈, 특수문자까지 그대로 사용
-
 fun main () {
+
+    // Char(문자 1개) //
+
+    // 글자 하나는 2bytes(16bits) 차지함
+
+    val alphabetA = 'A'
+    val koreanChar = 'ㄱ'
+
+
+
+    // Strings(문자열) //
+
+    // 큰따옴표 안에
+    // "이런 식으로"
+    // 사용
+    val oneString = "one line string test"
+
+    // 여러 줄인 경우
+    // """이런 식으로"""
+    // 줄바꿈, 특수문자까지 그대로 사용
+    val threeStrings = """one
+        =two=
+        three^^
+    """
 
     // + 연산자를 사용해 두 문자열 결합하기
     val language = "Kotlin"
