ed735372f478a714df263024281e2d688a4c4394
shinyelee <shinye0213@naver.com>
2022-03-31 23:14:14 +0900

    함수 보충설명

--- src/Basic06function.kt
+++ src/Basic06function.kt
@@ -1,6 +1,7 @@
 // function
 
 fun main() {
+
     testFun()
     abc()
     abc2()
@@ -10,7 +11,13 @@ fun main() {
     val sumValue = sum(30, 60)
     println(sumValue)
     cuckoo(50)
+
+    // 출력 안 하는 함수
+    getCardString("invu", 9)
+
 }
+
+
 
 fun cuckoo(time : Int) {
     println("$time 분 후에 취사가 완료됩니다.")
@@ -42,3 +49,14 @@ fun testFun() {
     println("c")
     println("d")
 }
+
+// 문자열 객체로 반환
+// fun getCardString(cardName: String, cardValue: Int): String {
+//     return("$cardName = $cardValue")
+// }
+
+// {}와 return 생략 가능
+// fun getCardString(cardName: String, cardValue: Int): String = "$cardName = $cardValue"
+
+// 반환타입(: String) 생략 가능
+fun getCardString(cardName: String, cardValue: Int) = "$cardName = $cardValue"
--- src/Beginner12function.kt
+++ src/Beginner12function.kt
@@ -1,44 +1,38 @@
-// Functions(함수)
+// Functions(함수) //
+
+// 특정한 동작이나 결과값 연산에 사용
+// 함수 생성시 함수명 앞에 fun을 붙임
 
 fun main() {
 
-    // 함수
+    // 문자열 출력하는 함수
     printJack()
-    add(1, 2, 3)
+//    Jack
 
-    // 매개변수 추가한 함수
+    // 매개변수 있는 함수
     printCard("ive", 11)
+//    ive = 11
 
-    // 출력 안 하는 함수
-    getCardString("invu", 9)
+    // 단일 표현식 함수
+    println(plus(4, 5, 6))
 
 }
 
-// 함수 생성
-fun printJack() {
-    println("Jack")
-}
+//코드 줄이기
+//fun plus(a: Int, b: Int, c: Int): Int {
+//    // 반환값
+//    return a + b + c
+//}
+// Single-Expression function(단일 표현식 함수) //
+// 반환형 추론 가능 -> return 생략
+fun plus(a: Int, b: Int, c: Int) = a + b + c
 
-// 매개변수 추가
+// () 안에 뭐 있음 -> 매개변수 있는 함수
 fun printCard(cardName: String, cardValue: Int) {
     println("$cardName = $cardValue")
 }
 
-// 문자열 객체로 반환
-fun getCardString(cardName: String, cardValue: Int): String {
-    return("$cardName = $cardValue")
+// () 비어있음 -> 매개변수 없는 함수
+fun printJack() {
+    println("Jack")
 }
-
-// single-expression function(단일 표현식 함수)
-//fun add(a: Int, b: Int, d: Int): Int {
-//    return a + b + c
-//}
-fun add(a: Int, b: Int, c: Int) = a + b + c
-
-// getCardString과 동일함
-// {}와 return 생략하고 = 씀
-fun getCardString2(cardName: String, cardValue: Int): String = "$cardName = $cardValue"
-
-// getCardString, getCardString2와 동일함
-// 반환타입(여기서는 : String) 생략 가능
-fun getCardString3(cardName: String, cardValue: Int) = "$cardName = $cardValue"
