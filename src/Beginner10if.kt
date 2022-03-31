b9ae72c2a228d4df528f5d29aeeeb11e6d1c954d
shinyelee <shinye0213@naver.com>
2022-03-31 23:47:02 +0900

    조건문 보충설명

--- src/Beginner10if.kt
+++ src/Beginner10if.kt
@@ -1,4 +1,6 @@
-// If Expression(if 표현식)
+// If Expression(if 표현식) //
+
+// if문의 조건이 참이면 -> 구문 실행
 
 fun main() {
 
@@ -6,12 +8,18 @@ fun main() {
     val a = 3
     val b = 5
     var aIsBigger = false
+
+    // if -> 조건을 만족할 때 구문 실행
     if (a > b) {
+        // a가 b보다 크면 true 출력
         aIsBigger = true
+    // else -> 조건을 만족하지 못할 때
     } else {
+        // a가 b보다 크지 않으면 false 출력
         aIsBigger = false
     }
     println(aIsBigger)
+//    false
 
     // 변수 설정
     val aIsBigger2 = if (a > b) {
@@ -20,10 +28,12 @@ fun main() {
         false
     }
     println(aIsBigger2)
+//    false
 
     // 실행할 구문이 하나라면
     // {} 생략 가능
-    val aIsBigger3 = if (a > b) true else false
+    val aIsBigger3 = if (a < b) true else false
     println(aIsBigger3)
+//    true
 
 }
