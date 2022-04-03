45f8f60c56a2e66d01e625a40f6b5e7022c6edbf
shinyelee <shinye0213@naver.com>
2022-04-03 14:23:34 +0900

    비교연산자 보충설명

--- src/Basic02ifElseWhen.kt
+++ src/Basic02ifElseWhen.kt
@@ -6,7 +6,6 @@
 fun main() {
 
     // if는 참/거짓 확인
-
     // if + else 조합이 보통임
     var studentScore1 = 100
     if (studentScore1 >= 150) {
@@ -15,6 +14,8 @@ fun main() {
         println("150 이상이 아님")
     }
 //    150 이상이 아님
+
+
 
     // if 구문을 여러 개 써도 됨
     // 둘 중 하나만 해당 -> 하나만 출력
@@ -27,6 +28,8 @@ fun main() {
     }
 //    50 이상
 
+
+
     // 둘 다 해당 -> 둘 다 출력
     var studentScore3 = 65
     if (studentScore3 < 70) {
@@ -37,6 +40,8 @@ fun main() {
     }
 //    70미만
 //    70이하
+
+
 
     // 여러 조건 중 하나만 얻어걸리면 된다 -> if else 사용
     var studentScore4 = 40
@@ -58,26 +63,26 @@ fun main() {
 
     // when 사용
     // 하나의 변수를 여러 개의 값과 비교 가능
-//    var score = 80
-//    when(score) {
-//        100 -> {
-//            println("100")
-//        }
-//        90 -> {
-//            println("90")
-//        }
-//        80 -> {
-//            println("80")
-//        }
-//        else -> {
-//            println("no")
-//        }
-//    }
+    var score1 = 80
+    when(score1) {
+        100 -> {
+            println("100")
+        }
+        90 -> {
+            println("90")
+        }
+        80 -> {
+            println("80")
+        }
+        else -> {
+            println("no")
+        }
+    }
 
     // 간단한 조건문을 이용한 문제풀이
     // 학생의 A B C D F 점수를 출력
 
-    var score = 80
+    var score2 = 80
 
     // 학생의 점수가
     // 100 초과 == A
@@ -86,13 +91,13 @@ fun main() {
     // 70 초과 == D
     // 70 이하 == F
 
-    if (score > 100) {
+    if (score2 > 100) {
         println("A")
-    } else if (score > 90) {
+    } else if (score2 > 90) {
         println("B")
-    } else if (score > 80) {
+    } else if (score2 > 80) {
         println("C")
-    } else if (score > 70) {
+    } else if (score2 > 70) {
         println("D")
     } else {
         println("F")
--- src/Bonus02comparisonOperators.kt
+++ src/Bonus02comparisonOperators.kt
@@ -1,5 +1,87 @@
+// Comparison Operators(비교연산자) //
+
+// 부등호(>, >=, <, <=)
+// 등호(!=, ==)
+// 할당 연산자(=)
+// is 연산자(is, !is)
+
 fun main() {
 
+    // a = b
+    // b를 a에 할당한다
+    var lucky = 7
+    // 7이라는 값을 lucky라는 변수에 넣겠다는 뜻
 
+    // a == b
+    // a는 b와 같다
+    if (lucky == 7) {
+        println("seven")
+    }
+//    seven
+
+    // a != b
+    // a는 b와 같지 않다
+    if (lucky != 13) {
+        println("not bad")
+    }
+//    not bad
+
+
+
+    // a > b
+    // a는 b보다 크다
+
+    // a >= b
+    // a는 b보다 크거나 같다
+
+    var score1 = 100
+    if (score1 >= 150) {
+        println("150 이상")
+    } else {
+        println("150 이상이 아님")
+    }
+//    150 이상이 아님
+
+
+
+    // a < b
+    // a는 b보다 작다
+
+    // a <= b
+    // a는 b보다 작거나 같다
+
+    var score2 = 70
+    if (score2 < 140) {
+        println("140미만")
+    }
+    if (score2 <= 70) {
+        println("70이하")
+    }
+//    140미만
+//    70이하
+
+
+
+    // a is String
+    // a의 자료형은 String이다
+    var type1 : Any = "abc"
+    if(type1 is String) {
+        println("this is string")
+    } else {
+        println("this is not string")
+    }
+//    this is string
+
+    // a !is Int
+    // a의 자료형은 Int가 아니다
+    var type2 : Any = "abc"
+    if(type2 !is Int) {
+        println("this is not int")
+    } else {
+        println("this is int")
+    }
+//    this is not int
+
+    // is 연산자는 나중에 Any, as 배울 때 재등장
 
 }
