118e6cf2e417c323b4042b9b8361049ea2f5b5b5
shinyelee <shinye0213@naver.com>
2022-04-05 00:27:59 +0900

    클래스 보충설명

--- src/Further04class.kt
+++ src/Further04class.kt
@@ -1,7 +1,11 @@
-// Class(클래스)
+// Class(클래스) //
+// 인스턴스를 만드는 틀
 
-// class == 값 + 기능
-// function == 기능
+// Instance(인스턴스) //
+// 클래스를 이용해 만들어내는 서로 다른 속성의 객체
+
+// 클래스 == 값(속성) + 기능(함수)
+// 함수 == 기능
 
 fun main() {
 
@@ -11,7 +15,7 @@ fun main() {
     var nmixx = Singer("설윤", 2004)
 
 //    println("안녕하세요, ${bts.birthYear}년생 ${bts.name}입니다.")
-    // 너무 번거로움
+    // 너무 번거로움 -> 아예 클래스 내에 함수로 넣어주면 됨
 
 //    bts.introduce()
 //    txt.introduce()
@@ -25,28 +29,30 @@ fun main() {
 
 }
 
-// 클래스 속성 && 생성자 선언
+// 클래스 속성(생성자) 선언
 //class Singer (var name: String, val birthYear: Int) {
 //    fun introduce() {
 //        println("안녕하세요, ${birthYear}년생 ${name}입니다.")
 //    }
 //}
-// constructor(생성자)
+// constructor(생성자) //
 // 새 인스턴스를 만들기 위해 호출하는 특수한 함수
 // 인스턴스 속성 초기화 && 인스턴스 생성시 구문 수행
 
-
+// () 안에 클래스가 가지는 속성 == 생성자
+// 함수 없이 속성만 가지는 클래스는 이 한 줄로 구현 끝!
 class Singer (var name: String, val birthYear: Int)
+// 중괄호 안에 함수 추가 가능
 {
-    // init
+    // init //
     // 파라미터/반환형 없는 함수
     // 생성자 -> 인스턴스 생성시 호출됨
     init {
-        // this
+        // this //
         // 인스턴스 자신의 속성이나 함수를 호출하기 위해 클래스 내에서 사용되는 키워드
         println("안녕하세요, ${this.birthYear}년생 ${this.name}입니다.")
     }
-    // secondary constructor(보조 생성자)
+    // secondary constructor(보조 생성자) //
     // 필요에 따라 추가적으로 선언
     // 기본 생성자와 다른 형태의 생성자 제공
     // -> 인스턴스 생성시 편의 제공 || 추가적인 구문 수행
