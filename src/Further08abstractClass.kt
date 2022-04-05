18cb0eac71d1538c77aacce926e5fc8497e276b5
shinyelee <shinye0213@naver.com>
2022-04-05 14:58:55 +0900

    추상화 보충설명

--- src/Further08abstractClass.kt
+++ src/Further08abstractClass.kt
@@ -1,11 +1,5 @@
-// Abstract(추상화)
+// Abstract(추상화) //
 // 형식만 선언하고 실제 구현은 서브 클래스에 일임할 때 사용하는 기능
-
-// Abstraction Function(추상 함수)
-// 선언부만 있고 기능이 구현되지 않은 함수
-
-// Abstraction Class(추상 클래스)
-// 추상 함수를 포함하는 클래스
 
 fun main() {
 
@@ -13,16 +7,24 @@ fun main() {
 
     r.lead()
     r.response()
+//    바니바니 바니바니
+//    당근 당근
 
 }
 
+// Abstraction Class(추상 클래스) //
+// 추상 함수를 포함하는 클래스
 abstract class Game {
-    // 얘는 추상 함수(껍데기만 있음)
+
+    // Abstraction Function(추상 함수) //
+    // 선언부만 있고 기능이 구현되지 않은 함수
     abstract fun response()
-    // 얘는 일반 함수(내용물이 있음)
+
+    // 일반 함수(기능이 있음)
     fun lead() {
         println("바니바니 바니바니")
     }
+
 }
 // 추상 클래스는 일부 함수가 구현되지 않은 미완성 클래스
 // -> 단독으로 인스턴스 만들기 불가능
@@ -30,8 +32,10 @@ abstract class Game {
 
 // Animal 상속 받음
 class Carot : Game() {
-    // eat 함수가 실제 동작되는 구현부
+
+    // response 함수가 실제 동작되는 구현부
     override fun response() {
         println("당근 당근")
     }
+
 }
