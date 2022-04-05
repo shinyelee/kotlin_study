d6e5277d00ba81018f79b95fc1f5d73e41541588
shinyelee <shinye0213@naver.com>
2022-04-05 23:06:10 +0900

    고차함수 보충설명

--- src/Advanced01lambda.kt
+++ src/Advanced01lambda.kt
@@ -4,10 +4,38 @@
 fun main() {
 
     bb(::aa)
+//    bb가 호출한 함수 aa
 
 //    val cc: (String)->Unit = {str: String -> println("$str 람다함수 cc")}
     // 이렇게 냅다 축약해도 됨
     val cc = {str: String -> println("$str 람다함수 cc")}
+
+    bb(cc)
+//    bb가 호출한 람다함수 cc
+
+
+
+    // 1. 여러 구문을 사용하는 람다 함수
+    val d = {str: String ->
+        println("$str 이런 식으로")
+        println("$str 여러 구문의")
+        println("$str 사용이 가능함")
+    }
+    val calculate: (Int, Int)->Int = {a, b ->
+        println(a)
+        println(b)
+        a+b
+    }
+    // 마지막 구문인 a+b의 값을 Int로 반환함
+
+    // 2. 파라미터가 없는 람다 함수
+    // -> 실행할 구문들만 나열
+    val e: ()->Unit = {println("파라미터 없음")}
+
+    // 3. 파라미터가 하나인 람다 함수
+    // -> it 사용(파라미터 이름 일일이 쓸 필요 X)
+    val f: (String)->Unit = {println("$it 파라미터 하나")}
+
 }
 
 fun aa (str: String) {
@@ -17,26 +45,3 @@ fun aa (str: String) {
 fun bb (function: (String)->Unit) {
     function("bb가 호출한")
 }
-
-// 설명 추가 //
-
-// 1. 여러 구문을 사용하는 람다 함수
-// val d = {str: String ->
-// println("$str 이런 식으로")
-// println("$str 여러 구문의")
-// println("$str 사용이 가능함")
-// }
-// val calculate: (Int, Int)->Int = {a, b ->
-// println(a)
-// println(b)
-// a+b
-// }
-// 마지막 구문인 a+b의 값을 Int로 반환함
-
-// 2. 파라미터가 없는 람다 함수
-// -> 실행할 구문들만 나열
-// val e: ()->Unit = {println("파라미터 없음")}
-
-// 3. 파라미터가 하나인 람다 함수
-// -> it 사용(파라미터 이름 일일이 쓸 필요 X)
-// val f: (String)->Unit = {println("$it 파라미터 하나")}
--- src/Advanced02higherOrderFunction.kt
+++ src/Advanced02higherOrderFunction.kt
@@ -1,10 +1,39 @@
-// Higher-order Function(고차함수)
+// Higher-order Function(고차함수) //
+
+// 일반 함수를 인자나 반환값으로 사용
 // 함수를 클래스에서 만들어낸 인스턴스처럼 취급하는 방법
 // 함수를 파라미터로 넘겨주거나 결과값으로 반환 받을 수 있음
 
 fun main() {
+
+    // 일반 인자
+    val res1 = sum1(3, 2)
+    // 3 + 2 ->  5
+    println(res1)
+//    5
+
+    // 인자에 함수 사용
+    val res2 = mul1(sum1(3,3), 3)
+    // (3 + 3) * 3 -> 18
+    println(res2)
+//    18
+
+    // 함수의 반환값으로 함수 사용
+    println("funcFunc: ${funcFunc()}")
+//    funcFunc: 4
+
     // :: -> 고차함수로 만드는 연산자
     b(::a)
+//    b가 호출한 함수 a
+}
+
+fun sum1(a: Int, b: Int) = a + b
+
+fun mul1(a: Int, b: Int) = a * b
+
+fun funcFunc(): Int {
+    // 함수의 반환값으로 함수 사용
+    return sum1(2, 2)
 }
 
 // 문자열을 받고 반환형은 없는 함수
@@ -12,6 +41,7 @@ fun a (str: String) {
     println("$str 함수 a")
 }
 
+// 함수를 자료형으로 나타내기
 // Unit -> 값이 없음
 fun b (function: (String)->Unit) {
     function("b가 호출한")
