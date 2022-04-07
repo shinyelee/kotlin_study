f50d10fb253c1520b34a8787666a48953eb85893
shinyelee <shinye0213@naver.com>
2022-04-07 14:11:51 +0900

    문자열 가공

--- src/Basic09StringFormat.kt
+++ src/Basic09StringFormat.kt
@@ -1,34 +1,77 @@
-// String Format 문자열 가공
+// String Format(문자열 가공) //
+
 // 유저가 입력한 데이터를 가공하거나
 // 서버에서 가져온 데이터를 알맞게 가공해서 쓰거나
 
 fun main() {
 
-//    val testString = "동해물과 백두산이 마르고 닳도록"
-//    println(testString[1])
-//    println(testString[2])
-//
-//    val a = testString[1]
-//    val b = testString[2]
-//    println(a.toString() + b.toString())
-//
-//    // [1]에서 시작, [3] 앞에서 자름
-//    val splitString = testString.substring(1, 3)
-//    println(splitString)
-//
-//    // 전체 가져오기
-//    val splitString2 = testString.substring(0, testString.length)
-//    println(splitString2)
-//
-//    // 공백(띄어쓰기) 기준으로 잘라줌
-//    val newTestString = testString.split(" ")
-//    println(newTestString)
-//    println(newTestString[0])
-//    println(newTestString[1])
-//
-//    // 문자열 대체하기
-//    val replaceValue = testString.replace("백두산", "한라산")
-//    println(replaceValue)
+    val testString = "동해물과 백두산이 마르고 닳도록"
+    println(testString[1])
+    println(testString[2])
+//    해
+//    물
+
+    val a = testString[1]
+    val b = testString[2]
+    println(a.toString() + b.toString())
+//    해물
+
+    // [1]에서 시작, [3] 앞에서 자름
+    val splitString = testString.substring(1, 3)
+    println(splitString)
+//    해물
+
+    // 전체 가져오기
+    val splitString2 = testString.substring(0, testString.length)
+    println(splitString2)
+//    동해물과 백두산이 마르고 닳도록
+
+    // 공백(띄어쓰기) 기준으로 잘라줌
+    val newTestString = testString.split(" ")
+    println(newTestString)
+    println(newTestString[0])
+    println(newTestString[1])
+//    [동해물과, 백두산이, 마르고, 닳도록]
+//    동해물과
+//    백두산이
+
+    // 문자열 대체하기
+    val replaceValue = testString.replace("백두산", "한라산")
+    println(replaceValue)
+//    동해물과 한라산이 마르고 닳도록
+
+
+
+    val test1 = "Test.Kotlin.String"
+
+    // 길이
+    println(test1.length)
+//    18
+
+    // 소문자
+    println(test1.toLowerCase())
+//    test.kotlin.string
+
+    // 대문자
+    println(test1.toUpperCase())
+//    TEST.KOTLIN.STRING
+
+    // . 기준으로 쪼개기
+    val test2 = test1.split(".")
+    println(test2)
+//    [Test, Kotlin, String]
+
+    // 문자열 합치기
+    println(test2.joinToString())
+    println(test2.joinToString("-"))
+//    Test, Kotlin, String
+//    Test-Kotlin-String
+
+    // 해당 부분만 출력
+    println(test1.substring(5..10))
+//    Kotlin
+
+
 
     // 문제
     val testList = ArrayList<String>()
