c128ce09dd79cf8d59634c14c0f26a4f03c5f30a
shinyelee <shinye0213@naver.com>
2022-04-09 00:50:54 +0900

    열거형 클래스

--- src/Advanced08enumClass.kt
+++ src/Advanced08enumClass.kt
@@ -1,49 +1,99 @@
-// Enum(enumerations) Class //
+// Enum(enumerated type) Class(열거형 클래스) //
+
+// 클래스 내 객체는 관행적으로 대문자로 기술
+// 클래스 내 객체는 고유한 속성 가짐(생성자 있으면 객체 선언시 속성 설정 가능)
+// 클래스 내 함수 기술시 마지막 객체 뒤에 세미콜론(;) 붙인 후 추가
 
 fun main() {
 
-//    println(Direction.NORTH)
-//    Direction.values().forEach {
-//        println(it)
-//    }
-//
-//    CustomerType.values().forEach {
-//        println(it)
-//    }
+    var state = State.SING
+    println(state)
+//    SING
 
-//    val direction = Direction.EAST
-//    when(direction) {
-//        Direction.NORTH -> {
-//            println("N")
-//        }
-//        Direction.SOUTH -> {
-//            println("S")
-//        }
-//        Direction.WEST -> {
-//            println("W")
-//        }
-//        Direction.EAST -> {
-//            println("E")
-//        }
-//    }
+    state = State.SLEEP
+    println(state.isSleeping())
+//    true
 
-//    val color = Color.GREEN
-//    when(color) {
-//        Color.RED -> {
-//            println(Color.RED.colorName)
-//        }
-//        Color.GREEN -> {
-//            println(Color.GREEN.colorName)
-//        }
-//        Color.BLUE -> {
-//            println(Color.BLUE.colorName)
-//        }
-//    }
+    state = State.EAT
+    println(state.message)
+//    밥을 먹습니다
+
+
+    println(Direction.NORTH)
+//    NORTH
+
+    Direction.values().forEach {
+        println(it)
+    }
+//    NORTH
+//    SOUTH
+//    WEST
+//    EAST
+
+
+
+    CustomerType.values().forEach {
+        println(it)
+    }
+//    A
+//    B
+//    C
+//    D
+//    VIP
+
+
+
+    val direction = Direction.EAST
+//    E
+
+    when(direction) {
+        Direction.NORTH -> {
+            println("N")
+        }
+        Direction.SOUTH -> {
+            println("S")
+        }
+        Direction.WEST -> {
+            println("W")
+        }
+        Direction.EAST -> {
+            println("E")
+        }
+    }
+
+
+
+    val color = Color.GREEN
+//    초록
+
+    when(color) {
+        Color.RED -> {
+            println(Color.RED.colorName)
+        }
+        Color.GREEN -> {
+            println(Color.GREEN.colorName)
+        }
+        Color.BLUE -> {
+            println(Color.BLUE.colorName)
+        }
+    }
+
+
 
     println(Device.DEVICE_ON.status)
+//    ON
+
     Device.DEVICE_ON.status = "OFF"
     println(Device.DEVICE_ON.status)
+//    OFF
 
+}
+
+enum class State(val message: String) {
+    SING("노래를 부릅니다"),
+    EAT("밥을 먹습니다"),
+    SLEEP("잠을 잡니다");
+    fun isSleeping() = this == State.SLEEP
 }
 
 enum class Device(var status : String) {
@@ -52,16 +102,16 @@ enum class Device(var status : String) {
     LOCATION("SEOUL")
 }
 
-//enum class Color(val colorName : String) {
-//    RED("빨강"),
-//    GREEN("초록"),
-//    BLUE("파랑")
-//}
+enum class Color(val colorName : String) {
+    RED("빨강"),
+    GREEN("초록"),
+    BLUE("파랑")
+}
 
-//enum class Direction {
-//    NORTH, SOUTH, WEST, EAST
-//}
+enum class Direction {
+    NORTH, SOUTH, WEST, EAST
+}
 
-//enum class CustomerType {
-//    A, B, C, D, VIP
-//}
+enum class CustomerType {
+    A, B, C, D, VIP
+}
