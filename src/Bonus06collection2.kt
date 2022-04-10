11f9c2af36046369d4853cc6c73ee392ff6d12a5
shinyelee <shinye0213@naver.com>
2022-04-10 22:54:21 +0900

    컬렉션 함수 (2)-2

--- src/Bonus06collection2.kt
+++ src/Bonus06collection2.kt
@@ -28,4 +28,29 @@ fun main () {
 //    [Gfriend(name=은비, birthYear=98), Gfriend(name=예원, birthYear=98)]
 //    [Gfriend(name=소정, birthYear=95), Gfriend(name=예린, birthYear=96), Gfriend(name=은비, birthYear=97), Gfriend(name=유나, birthYear=97)]
 
+
+
+    val numbers = listOf(-3, 7, 2, -10, 1)
+
+    // flatMap //
+    // 아이템마다 만들어진 컬렉션을 합쳐서 반환하는 함수
+    println(numbers.flatMap { listOf(it * 10, it + 10) })
+//    [-30, 7, 70, 17, 20, 12, -100, 0, 10, 11]
+
+    // getOrElse //
+    // 인덱스 위치에 아이템이 있으면 아이템을 반환
+    // -> 없으면 지정한 기본값을 반환
+    println(numbers.getOrElse(1) { 50 })
+    println(numbers.getOrElse(10) { 50 })
+//    7
+//    50
+
+    val names = listOf("A", "B", "C", "D")
+
+    // zip //
+    // 컬렉션 두 개의 아이템을 1:1로 매칭해 새 컬렉션을 만듦
+    // -> 결과 List의 아이템 개수는 더 작은 컬렉션을 따라감
+    println(names zip numbers)
+//    [(A, -3), (B, 7), (C, 2), (D, -10)]
+
 }
