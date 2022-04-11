532cf1d629f9572d7d4d5024958bfd5f6541d1ce
shinyelee <shinye0213@naver.com>
2022-04-11 11:03:59 +0900

    map 주석추가

--- src/Beginner08map.kt
+++ src/Beginner08map.kt
@@ -2,27 +2,35 @@
 
 fun main() {
 
-    // 키-값 저장
+    // key(키)-value(값) 저장
     val cards = mapOf("Jack" to 11, "Queen" to 12, "King" to 13)
     println(cards)
+//    {Jack=11, Queen=12, King=13}
 
     // 키 사용해 값 검색
     val jackValue = cards["Jack"]
     println(jackValue)
+//    11
 
     // mutable map이어야
     val cards2 = mutableMapOf("Jack" to 11, "Queen" to 12, "King" to 13)
     println(cards2)
-    // 아이템 추가 가능
+//    {Jack=11, Queen=12, King=13}
+
+    // 아이템 추가 및 삭제 가능
     cards2["Ace"] = 1
     println(cards2)
+//    {Jack=11, Queen=12, King=13, Ace=1}
 
 
     // mutable map으로 변환하기
     val cards3 = mapOf("Jack" to 11, "Queen" to 12, "King" to 13)
     println(cards3)
+//    {Jack=11, Queen=12, King=13}
+
     val mutableCards = cards.toMutableMap()
     mutableCards["Ace"] = 1
     println(mutableCards)
+//    {Jack=11, Queen=12, King=13, Ace=1}
 
 }
