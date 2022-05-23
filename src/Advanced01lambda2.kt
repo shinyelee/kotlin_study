f10ac6d51d30a9da5606eee14d38cb7b2ef3f1fe
shinyelee <shinye0213@naver.com>
2022-05-23 23:49:40 +0900

    람다 보충설명 (1) 주석 수정

--- src/Advanced01lambda2.kt
+++ src/Advanced01lambda2.kt
@@ -6,7 +6,7 @@ fun main() {
 //    144
 
     println(triangle(4))
-//
+//    8
 
     println(book("작별인사", 2))
 //    <작별인사> 2권 주세요
@@ -17,11 +17,11 @@ fun main() {
 }
 
 // 기본형
-// val 람다함수명 : (input 자료형) -> (output 자료형) = { 인자 -> 코드 }
+// val 람다함수명 : (input 자료형) -> (output 자료형) = { 매개변수 -> 본문 }
 val square : (Int) -> (Int) = { number -> number * number }
 
 // 좀 더 짧게
-// val 람다함수명 = { 인자 : 자료형 -> 코드 }
+// val 람다함수명 = { 매개변수 : 자료형 -> 본문 }
 val triangle = { number : Int -> number * number / 2 }
 
 // 기본형2
@@ -29,7 +29,11 @@ val book : (String, Int) -> String = { title : String, many : Int ->
     "<${title}> ${many}권 주세요"
 }
 
-// 짧게2
+// 좀 더 짧게2
 val album = {title : String, many : Int ->
     "<${title}> ${many}장 주세요"
 }
+
+// 람다함수는 value처럼 다룰 수 있는 익명함수
+// 1. 메소드의 매개변수로 넘겨줄 수 있음
+// 2. 리턴값으로 쓸 수 있음
