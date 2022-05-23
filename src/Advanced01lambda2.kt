dcb7aadb88abf48cfbf3e07afa50a83a0d62ead1
shinyelee <shinye0213@naver.com>
2022-05-23 23:56:53 +0900

    람다 보충설명 (2) 확장함수

--- src/Advanced01lambda2.kt
+++ src/Advanced01lambda2.kt
@@ -1,6 +1,8 @@
-// 람다 보충설명 (1)
+// 람다 보충설명 (2)
 
 fun main() {
+
+    // 람다함수 //
 
     println(square(12))
 //    144
@@ -13,6 +15,22 @@ fun main() {
 
     println(album("PROOF", 1))
 //    <PROOF> 1장 주세요
+
+
+
+    // 확장함수 //
+
+    val a = "I love my"
+    val b = "Love your"
+
+    println(a.self())
+//    I love myself:)
+
+    println(b.self())
+//    Love yourself:)
+
+    println(extendString("you", 3000))
+//    I love you 3000
 
 }
 
@@ -37,3 +55,25 @@ val album = {title : String, many : Int ->
 // 람다함수는 value처럼 다룰 수 있는 익명함수
 // 1. 메소드의 매개변수로 넘겨줄 수 있음
 // 2. 리턴값으로 쓸 수 있음
+
+
+
+// 확장함수 //
+
+// String.(매개변수 없음)
+val self : String.() -> String = {
+
+    // String 오브젝트 그 자체를 this로 가리킴
+    this + "self:)"
+
+}
+
+fun extendString(who : String, much : Int) : String {
+
+    // String.(매개변수 있음) <- this
+    // 매개변수 Int를 it으로 가리킴
+    val ironMan : String.(Int) -> String = { "I love ${this} ${it}" }
+
+    return who.ironMan(much)
+
+}
