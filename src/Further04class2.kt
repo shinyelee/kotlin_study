c1b2d0f66e2964adf83fa9994f08d9c73484c5a8
shinyelee <shinye0213@naver.com>
2022-05-23 15:56:04 +0900

    상속 보충설명 (3)

--- src/Further04class2.kt
+++ src/Further04class2.kt
@@ -1,12 +1,13 @@
-// 상속 보충설명 (2)
+// 상속 보충설명 (3)
 
 fun main() {
 
     val korean = Korean()
 //    인스턴스 생성할 때 함께 실행됨
 
+    // 상속됨
     korean.singASong()
-//    상속 전 : lalala
+//    상속 후 : 라라라
 
 }
 
@@ -24,8 +25,7 @@ open class Human(val name : String = "anonymous") {
         println("yummy!")
     }
 
-    // 자바와 달리 코틀린에서는 상속해 주는 function 앞에 open을 붙여줘야 함
-    fun singASong() {
+    open fun singASong() {
         println("상속 전 : lalala")
     }
 
@@ -33,9 +33,8 @@ open class Human(val name : String = "anonymous") {
 
 class Korean : Human() {
 
-//    Kotlin: 'singASong' in 'Human' is final and cannot be overridden
-//    override fun singASong() {
-//        println("상속 후 : 라라라")
-//    }
+    override fun singASong() {
+        println("상속 후 : 라라라")
+    }
 
 }
