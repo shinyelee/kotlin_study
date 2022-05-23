5b73fe01df32015e790dad51609038ab2b431b40
shinyelee <shinye0213@naver.com>
2022-05-23 16:01:21 +0900

    상속 보충설명 (4)

--- src/Further04class2.kt
+++ src/Further04class2.kt
@@ -1,12 +1,12 @@
-// 상속 보충설명 (3)
+// 상속 보충설명 (4)
 
 fun main() {
 
     val korean = Korean()
 //    인스턴스 생성할 때 함께 실행됨
 
-    // 상속됨
     korean.singASong()
+//    상속 전 : lalala
 //    상속 후 : 라라라
 
 }
@@ -34,6 +34,10 @@ open class Human(val name : String = "anonymous") {
 class Korean : Human() {
 
     override fun singASong() {
+        // 상속 전 메서드와 상속 후 메서드를 모두 쓰고 싶다면
+        // super.메서드명
+        // 형태로 활용하면 됨
+        super.singASong()
         println("상속 후 : 라라라")
     }
 
