b817127cdc59a96c6bd4442758b5b6ca3c31b2b4
shinyelee <shinye0213@naver.com>
2022-05-26 23:15:17 +0900

    함수 공식문서 (2) 기본 인자, 지명 인자

--- src/Further05overloadNamedargVaragInfixfunc.kt
+++ src/Further05overloadNamedargVaragInfixfunc.kt
@@ -2,12 +2,16 @@
 
 // 같은 스코프 안에서 같은 이름의 함수를 여러 개 만들 수 있는 기능
 
-// 이름이 같고 파라미터의 자료형이 다르면 오버로딩 가능
+// 이름이 같고 파라미터(매개변수)의 자료형이 다르면 오버로딩 가능
 // 이름이 같고 파라미터의 개수가 다르면 오버로딩 가능
 // 이름이 같고 파라미터의 자료형과 개수가 같으면 오버로딩 불가능
 
 fun main() {
 
+    // 만약 기본 인자가 있다면 아래처럼 호출 가능
+//    read(x)
+
+    // 기본 인자가 없음
     read(7)
     read("감사합니다")
 //    숫자 7 입니다
@@ -15,6 +19,7 @@ fun main() {
 
 
 
+    // 꼭 모든 인자를 쓸 필요는 없음
     deliveryItem("짬뽕")
     deliveryItem("책", 4)
     deliveryItem("노트북", 1, "학교")
@@ -25,11 +30,22 @@ fun main() {
     //    deliveryItem("선물",  "친구집")
 //    Kotlin: Type mismatch: inferred type is String but Int was expected
 
-    // Named Argument(지명 인자) //
+    // Named Argument(명명된 인자, 지명 인자) //
+
+    // 파라미터 개수가 많거나, 기본값이 많을 때 가독성을 높여줌
     // 파라미터의 순서와 관련없이 파라미터의 이름을 사용해 직접 파라미터 값 할당
     // -> count 생략해도 정상적으로 실행됨
     deliveryItem("선물",  destination = "친구집")
 //    선물, 2개를 친구집에 배달 완료
+
+    Gfriend().umji()
+//    엄지의 데뷔일은 20150116
+    Viviz().umji()
+//    재데뷔했어도 엄지의 데뷔일은 여전히 20150116
+
+    // first의 기본값이 사용됨
+    apple(second = 2)
+//    first : 1, second : 2
 
 
 
@@ -64,9 +80,36 @@ fun read(x: String) {
 
 
 
+// Default Arguments(기본 인자, 파라미터의 기본값) //
+
+// (파라미터의) 기본값이 있으면 함수 호출시 해당 인자를 건너뛸 수 있음
+// (따로 써넣지 않아도 기본값으로 세팅되기 때문에)
+// -> 다른 언어에 비해 과부하 줄어듦
 fun deliveryItem(name: String, count: Int = 2, destination: String = "집") {
     println("${name}, ${count}개를 ${destination}에 배달 완료")
 }
+// 매개변수: 자료형 = 기본값
+// 의 형태를 가짐(예시에선 count, destination)
+
+// 기본 메서드의 기본값을
+open class Gfriend {
+    open fun umji(debut : Int = 20150116) {
+        println("엄지의 데뷔일은 $debut")
+    }
+}
+// 오버라이딩 메서드가 그대로 사용하기 때문에
+class Viviz : Gfriend() {
+    override fun umji(debut: Int) {
+        println("재데뷔했어도 엄지의 데뷔일은 여전히 $debut")
+    }
+}
+// 오버라이딩 메서드에서는 해당 파라미터의 기본값을 따로 가질 수 없음
+
+// 기본값이 있는 파라미터가 기본값이 없는 파라미터 앞에 위치하면
+fun apple(first: Int = 1, second: Int) {
+    println("first : $first, second : $second")
+}
+// 기본값은 지명 인자로 함수를 호출해야만 사용할 수 있음
 
 
 
